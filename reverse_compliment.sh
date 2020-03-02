7a33701204d12d1eab3aa3ec252609b9708a11bb
Ram Sai Nanduri <35064914+ramsainanduri@users.noreply.github.com>
2020-03-02 11:18:30 +0530

    Add files via upload

--- reverse_compliment.sh
+++ reverse_compliment.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/sh
 #Script developed by Ram Sai Nanduri
 #usage: sh reverse_compliment.sh input.txt
-
+#Copy the script and input file to current working directory and run it
 
 input_seq=$1 #input text file containing each sequences on a new line
 wd=$(pwd)
 seq=$(cat $wd/$input_seq | sed 's/\n\r/\s/g' )
 
-#echo $seq
+if [ -f $wd/output.txt ]
+then
+	rm $wd/output.txt
+fi
 
 for i in $seq
 do
@@ -19,7 +22,6 @@ if [ -f $wd/reverse_compliment_output.txt ]
 then
 	rm $wd/reverse_compliment_output.txt
 fi
-
 
 echo -e "Raw_Seq\tReverse Complimented Seq" > $wd/reverse_compliment_output.txt
 paste $wd/$input_seq $wd/output.txt >> $wd/reverse_compliment_output.txt
